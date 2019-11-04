fbe3f6cc06781b4acc179d702e4a6463d4514d30
Chuck Stanski <chuck.stanski@light.co>
2019-11-04 11:37:13 -0500

    Bug fix for list that are not the same size Added an edge filter
    Change-Id: I88312a860d3c785ddb331d80121625b5bd88e4cb

--- misc_scripts/cal_distortion_map_group.py
+++ misc_scripts/cal_distortion_map_group.py
@@ -20,13 +20,38 @@ matplot.use('TkAgg')
 import matplotlib.pyplot as plt
 
 
+def filter_corners(corners, xip, nx, ny, setup_info, remove_cols=None, remove_rows=None, remove_edge_percent=0.0):
+    corners = corners.reshape(ny, nx, 1, 2)
+    xip = xip.reshape(ny, nx, 1, 2)
+
+    if remove_rows is not None:
+        corners = np.delete(corners, remove_rows, axis=0)
+        xip = np.delete(xip, remove_rows, axis=0)
+
+    if remove_cols is not None:
+        corners = np.delete(corners, remove_cols, axis=1)
+        xip = np.delete(xip, remove_cols, axis=1)
+
+    # Remove outer edge
+    corners = corners.reshape(-1, 1, 2)
+    xip = xip.reshape(-1, 1, 2)
+    edge = remove_edge_percent / 100.0
+    x1 = np.greater(corners[:, 0, 0], setup_info.SensorInfo.width * edge)
+    x2 = np.less(corners[:, 0, 0], setup_info.SensorInfo.width * (1.0 - edge))
+    y1 = np.greater(corners[:, 0, 1], setup_info.SensorInfo.height * edge)
+    y2 = np.less(corners[:, 0, 1], setup_info.SensorInfo.height * (1.0 - edge))
+    i = np.all(np.vstack((x1, x2, y1, y2)), axis=0)
+    return corners[i, :, :], xip[i, :, :]
+
+
+
 ####################
 # Input Parameters
 ####################
 
-parser = argparse.ArgumentParser(description="Homography Test")
-parser.add_argument('--image_dir', default='tv_86in_circlegrid_oct18')
-parser.add_argument('--cal_dir', default=None)
+parser = argparse.ArgumentParser(description="Calibration Distortion Map Group")
+parser.add_argument('--image_dir', default='A1_distortion_oct24_01')
+parser.add_argument('--cal_dir', default='A1_distortion_oct24_01')
 parser.add_argument('--use_distortion_from', default=None)
 
 args, unknown = parser.parse_known_args()
@@ -42,7 +67,9 @@ display_size = image_helper.display_size(1024)
 setupInfo = image_helper.setup_info()
 
 center = np.array([setupInfo.SensorInfo.width * 0.5, setupInfo.SensorInfo.height * 0.5])
-max_dist = setupInfo.SensorInfo.width * 1.0 / 1.0
+max_dist = setupInfo.SensorInfo.width * 1.0 / 16.0
+
+filter_edge_percentage = 10.0
 
 plt.figure(0)
 
@@ -129,6 +156,7 @@ obj_pts = []
 img_pts = []
 H = []
 distortion_error = []
+distortion_xip = []
 dist_map = []
 for group in range(len(find_ret)):
     # Gather data to compute the homography
@@ -152,6 +180,11 @@ for group in range(len(find_ret)):
                                                   0])
             pose_idx = setupInfo.ChartInfo.pose_info[group]['pose_nx'] * pose_y + pose_x
             corners = find_corners[group][pose_idx].copy()
+
+            # Filter
+            #xip = np.delete(obj_corners.reshape(-1, 1, 3), 2, axis=2)
+            #filter_corners(corners, xip, nx, ny, setupInfo, [0, nx-1], [0, ny-1], filter_edge_percentage)
+            #obj_corners = np.insert(xip, 2, 0, axis=2).reshape(-1, 3)
 
             # Undistort
             if args.use_distortion_from is not None:
@@ -183,6 +216,7 @@ for group in range(len(find_ret)):
 
     # Compute the errors per corner
     distortion_error.append([])
+    distortion_xip.append([])
     pose_x = 0
     pose_y = 0
     nx = setupInfo.ChartInfo.pose_info[group]['nx']
@@ -197,12 +231,16 @@ for group in range(len(find_ret)):
             idx = setupInfo.ChartInfo.pose_info[group]['pose_nx'] * pose_y + pose_x
             corners = find_corners[group][idx].copy()
 
+            # Remove outer edge
+            #corners, xip = filter_corners(corners, xip, nx, ny, setupInfo, [0, nx-1], [0, ny-1], filter_edge_percentage)
+
             # Undistort
             if args.use_distortion_from is not None:
                 corners = lens_distortion.correct_distortion_points(corners)
 
             error = corners - xip
             distortion_error[group].append(np.array(error))
+            distortion_xip[group].append(np.array(xip))
             p = plt.figure(group)
             p.clear()
             x = xip[:, 0, 0]
@@ -227,6 +265,7 @@ for group in range(len(find_ret)):
         else:
             print("Group: {}, Pose: {}, {} - Checkerboard not found ".format(group, pose_x, pose_y))
             distortion_error[group].append(np.zeros((nx * ny, 1, 2)))
+            distortion_xip[group].append(np.zeros((nx * ny, 1, 2)))
 
         pose_x += 1
         if pose_x >= setupInfo.ChartInfo.pose_info[group]['pose_nx']:
@@ -236,9 +275,10 @@ for group in range(len(find_ret)):
     if args.use_distortion_from is None:
         # Compute the distortion map
         dist_error = np.array(distortion_error[group])
+        dist_xip = np.array(distortion_xip[group])
         img_shape = (setupInfo.SensorInfo.height, setupInfo.SensorInfo.width)
-        c = np.squeeze(find_corners[group][find_ret[group]], axis=2).reshape(-1, 2)
-        e = np.squeeze(dist_error[find_ret[group]], axis=2).reshape(-1, 2)
+        c = np.squeeze(np.concatenate([np.array(x) for x in dist_xip[find_ret[group]]])).reshape(-1, 2)
+        e = np.squeeze(np.concatenate([np.array(d) for d in dist_error[find_ret[group]]])).reshape(-1, 2)
 
         dist_map.append([])
         dist_map[group] = lens_distortion.compute_distortion_map(img_shape, c, e)
@@ -259,11 +299,11 @@ if args.use_distortion_from is None:
 
     # FIXME(Chuck) - Need to combine all distortion maps into a single map. simply combine for not but should align
     img_shape = (setupInfo.SensorInfo.height, setupInfo.SensorInfo.width)
-    c = np.squeeze(find_corners[0][find_ret[0]], axis=2).reshape(-1, 2)
-    e = np.squeeze(np.array(distortion_error[0])[find_ret[0]], axis=2).reshape(-1, 2)
+    c = np.squeeze(np.vstack([np.array(x) for x in np.array(distortion_xip[0])[find_ret[0]]])).reshape(-1, 2)
+    e = np.squeeze(np.vstack([np.array(d) for d in np.array(distortion_error[0])[find_ret[0]]])).reshape(-1, 2)
     for group in range(1, len(dist_map)):
-        c = np.concatenate((c, np.squeeze(find_corners[group][find_ret[group]], axis=2).reshape(-1, 2)))
-        e = np.concatenate((e, np.squeeze(np.array(distortion_error[group])[find_ret[group]], axis=2).reshape(-1, 2)))
+        c = np.vstack((c, np.squeeze(np.vstack([np.array(x) for x in np.array(distortion_xip[group])[find_ret[group]]])).reshape(-1, 2)))
+        e = np.vstack((e, np.squeeze(np.vstack([np.array(d) for d in np.array(distortion_error[group])[find_ret[group]]])).reshape(-1, 2)))
 
         lens_distortion.set_distortion_map(dist_map[group])
         lens_distortion.plot_distortion_map(30 + group, 64)
